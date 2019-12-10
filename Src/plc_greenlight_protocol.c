af14c02d076de33aff2d17430741b7567fe3427b
krepak <krepakvitaliy@gmail.com>
2019-12-10 23:21:35 +0300

    [soft] start developing of genereal greenlight protocol funcitons

--- Src/plc_greenlight_protocol.c
+++ Src/plc_greenlight_protocol.c
@@ -13,4 +13,25 @@
   */
   
 #include "plc_greenlight_protocol.h"
+#include "crc.h"
+
+PacketStatus Get_Regular_Packet     (uint8_t * buf, uint8_t buf_size)
+{
+  
+}
+
+PacketStatus Get_Maintenance_Packet (uint8_t * buf, uint8_t buf_size);
+PacketStatus Get_Repeater_Packet    (uint8_t * buf, uint8_t buf_size);
+
+
+PacketStatus Is_Regular_Packet (uint8_t * buf, uint8_t buf_size)
+{
+    uint32_t calc_crc = HAL_CRC_Calculate(&hcrc, 
+                        (uint32_t*)buf, 
+                        MAINTENANCE_PACKET_SIZE-MAINTENANCE_PACKET_CRC_SIZE);
+}
+
+
+PacketStatus Is_Maintenance_Packet (uint8_t * buf, uint8_t buf_size);
+PacketStatus Is_Repeater_Packet    (uint8_t * buf, uint8_t buf_size);
 
